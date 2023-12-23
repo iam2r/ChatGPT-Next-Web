04cde67494a4e0906bf77c675813f59558e9ac76
iamrazo <bestwishtous@gmail.com>
2023-12-24 04:16:20 +0800

    feature: 大小写

--- app/api/recognize/[...path]/route.ts
+++ app/api/recognize/[...path]/route.ts
@@ -3,12 +3,12 @@ import { getServerSideConfig } from "@/app/config/server";
 const handle = async (req: Request) => {
   const formData = await req.formData();
   const { GOOGLE_GEMININ_API_KEY } = getServerSideConfig();
+  const headers = new Headers();
+  headers.append("authorization", `Bearer ${GOOGLE_GEMININ_API_KEY}`);
   return fetch("https://openai-iamrazo.koyeb.app/recognize", {
     method: "POST",
     body: formData,
-    headers: {
-      Authorization: `Bearer ${GOOGLE_GEMININ_API_KEY}`,
-    },
+    headers,
   });
 };
 
